a8264d96b80d39c3f866cbe53fea4880e7efad43
夏小吉 <827798905@qq.com>
2023-08-15 21:23:58 +0800

    init;11

---
+++ src/assets/images/course/index.js
@@ -0,0 +1,34 @@
+import course1Banner1 from "@/assets/images/course1/banner1";
+import course1Banner2 from "@/assets/images/course1/banner2";
+
+import course2Banner1 from "@/assets/images/course2/banner1";
+import course2Banner2 from "@/assets/images/course2/banner2";
+
+import course3Banner1 from "@/assets/images/course3/banner1";
+import course3Banner2 from "@/assets/images/course3/banner2";
+
+import course4Banner1 from "@/assets/images/course4/banner1";
+import course4Banner2 from "@/assets/images/course4/banner2";
+
+import course5Banner1 from "@/assets/images/course5/banner1";
+import course5Banner2 from "@/assets/images/course5/banner2";
+
+import course6Banner1 from "@/assets/images/course6/banner1";
+import course6Banner2 from "@/assets/images/course6/banner2";
+
+import course7Banner1 from "@/assets/images/course7/banner1";
+import course7Banner2 from "@/assets/images/course7/banner2";
+
+import course8Banner1 from "@/assets/images/course8/banner1";
+import course8Banner2 from "@/assets/images/course8/banner2";
+
+export default {
+  course1: { course1Banner1, course1Banner2 },
+  course2: { course2Banner1, course2Banner2 },
+  course3: { course3Banner1, course3Banner2 },
+  course4: { course4Banner1, course4Banner2 },
+  course5: { course5Banner1, course5Banner2 },
+  course6: { course6Banner1, course6Banner2 },
+  course7: { course7Banner1, course7Banner2 },
+  course8: { course8Banner1, course8Banner2 },
+}
